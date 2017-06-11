7ea06b6894a022718443cd5512fbee22b9e91111
Adellhk <adell.hk@gmail.com>
2017-06-11 13:32:03 -0700

    Scaffold weatherutility

--- app/src/main/java/io/adell/aerisweatherapp/MainActivity.java
+++ app/src/main/java/io/adell/aerisweatherapp/MainActivity.java
@@ -1,12 +1,23 @@
 package io.adell.aerisweatherapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+import io.adell.aerisweatherapp.utilities.WeatherUtility;
 
 public class MainActivity extends AppCompatActivity {
 
+  private String ZIP = "11101";
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+
+  @Override public View onCreateView(String name, Context context, AttributeSet attrs) {
+    View v = super.onCreateView(name, context, attrs);
+    WeatherUtility.getForecast(context, ZIP);
+    return v;
   }
 }
---
+++ app/src/main/java/io/adell/aerisweatherapp/utilities/WeatherUtility.java
@@ -0,0 +1,54 @@
+package io.adell.aerisweatherapp.utilities;
+
+import android.content.Context;
+import android.util.Log;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Adell on 6/11/2017.
+ */
+
+public class WeatherUtility {
+  private static String clientId = "BayMxU3cAiWqpt97jHhhL";
+  private static String clientSecret = "yjoCIjdwKcZRC4NZEhL7p8s12DpGRl3SzroYfytm";
+  private static String aerisUrl = "http://api.aerisapi.com/";
+
+  public static void getForecast(Context context, String zip) {
+    JsonObjectRequest forecastRequest =
+        new JsonObjectRequest(Request.Method.GET, forecastsUrl(zip), null,
+            new Response.Listener<JSONObject>() {
+              @Override public void onResponse(JSONObject response) {
+                Log.d("WeatherUtility", "onResponse: " + response);
+                try {
+                  response.getJSONArray("response");
+                } catch (JSONException e) {
+                  e.printStackTrace();
+                }
+              }
+            }, new Response.ErrorListener() {
+          @Override public void onErrorResponse(VolleyError error) {
+            Log.d("WeatherUtility", "onErrorResponse: " + error);
+          }
+        });
+
+    RequestQueue queue = Volley.newRequestQueue(context);
+    queue.add(forecastRequest);
+  }
+
+  private static String forecastsUrl(String zip) {
+    return aerisUrl
+        + "forecasts/"
+        + zip
+        + "?client_id="
+        + clientId
+        + "&client_secret="
+        + clientSecret;
+  }
+}
